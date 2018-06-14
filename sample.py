68995b4eb60104ab52383c74d8d4816f3469d9ed
Suchendra <suchendrakuchipudi@gmail.com>
2018-06-13 20:23:02 -0700

    file added

---
+++ sample.py
@@ -0,0 +1,36 @@
+import csv
+import json
+
+
+class TestClass(object):
+
+    def __init__(self, foo, bar, baz):
+        self.foo = foo
+        self.bar = bar
+        self.baz = baz
+
+    def fizz_buzz(self, digit_1, digit_2):
+        for i in range(1, 100):
+            if i % digit_1 == 0:
+                if i % digit_2 == 0:
+                    print 'fizzbuzz!'
+                else:
+                    print 'fizz!'
+            elif i % digit_2 == 0:
+                print 'buzz!'
+            else:
+                print i
+
+    def json_to_csv(self, json_file_path, outfile_path):
+        """Convert a file containing a list of flat JSON objects to a csv.
+
+        What's a DictWriter, you say? Never heard of it!
+
+        """
+        with open(json_file_path) as f:
+            data = json.load(f)
+        with open(outfile_path, 'w') as fp:
+            writer = csv.writer(fp)
+            writer.writerow(data[0].keys())
+            for item in data:
+                writer.writerow(item.values())
