dbb5a2004673fb0d6b7ed3d7335a5c3bf9ba7eed
Suchendra <suchendrakuchipudi@gmail.com>
2018-06-13 20:24:58 -0700

    added hello

--- sample.py
+++ sample.py
@@ -29,8 +29,11 @@ class TestClass(object):
             for j in range(10):
                 print i * j
 
+    def hello():
+        print "hello"
+
     @staticmethod
-    def json_to_csv(json_file_path, outfile_path):
+    def json_to_csv(self, json_file_path, outfile_path):
         """Convert a file containing a list of flat JSON objects to a csv.
 
         What's a DictWriter, you say? Never heard of it!
