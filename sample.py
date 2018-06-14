06e6a2577dcb0173ff4ef83c87e4e1728afe74bb
Suchendra <suchendrakuchipudi@gmail.com>
2018-06-13 20:24:02 -0700

    file changed

--- sample.py
+++ sample.py
@@ -9,7 +9,8 @@ class TestClass(object):
         self.bar = bar
         self.baz = baz
 
-    def fizz_buzz(self, digit_1, digit_2):
+    @staticmethod
+    def fizz_buzz(digit_1, digit_2):
         for i in range(1, 100):
             if i % digit_1 == 0:
                 if i % digit_2 == 0:
@@ -21,7 +22,15 @@ class TestClass(object):
             else:
                 print i
 
-    def json_to_csv(self, json_file_path, outfile_path):
+    def oh_this_is_trouble(self, param1, param2):
+        """Oh man this fucntion is trouble"""
+        for i in range(20):
+            print 'trouble' + i * '!'
+            for j in range(10):
+                print i * j
+
+    @staticmethod
+    def json_to_csv(json_file_path, outfile_path):
         """Convert a file containing a list of flat JSON objects to a csv.
 
         What's a DictWriter, you say? Never heard of it!
@@ -29,8 +38,13 @@ class TestClass(object):
         """
         with open(json_file_path) as f:
             data = json.load(f)
+        fp = StringIO()
         with open(outfile_path, 'w') as fp:
             writer = csv.writer(fp)
             writer.writerow(data[0].keys())
             for item in data:
                 writer.writerow(item.values())
+
+if __name__ == '__main__':
+    t = TestClass(1, 2, 3)
+    TestClass.fizz_buzz(3, 5)
